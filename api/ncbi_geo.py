d7af7007af88dfc34c4f6d2f4caa0585437ba481
Jerry <xzhou@scripps.edu>
2020-01-17 13:04:53 -0800

    Attempt to auto recognize request protocol

--- api/ncbi_geo.py
+++ api/ncbi_geo.py
@@ -109,7 +109,8 @@ class NCBIProxyHandler(tornado.web.RequestHandler):
     '''
 
     def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", options.server_host)
+        self.set_header("Access-Control-Allow-Origin",
+                        self.request.protocol + '://' + options.server_host)
         self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
 
